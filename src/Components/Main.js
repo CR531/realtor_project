66668f4960da27b12229ad62dfcd6afd573cdcbe
Reddy531 <chakradhar.lsg4@gmail.com>
2021-01-08 00:45:28 -0600

    Added nav bar with titles

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../CSS/project.css"
 class Main extends React.Component {
     render() {
+        const { EarthquakeData } = this.props;
         return (
-            <div>
-                Realtor Main Page
-            </div>
+            <Router>
+                <nav className="navbar navbar-expand-lg navbar-light" style={{ "background": "lightgrey" }}>
+                    <div>
+                        <ul className="navbar-nav mr-auto">
+                            <li><Link to={'/'} className="nav-link">
+                                <img className="realtor_logo" src={EarthquakeData.site.logoImage} alt="Realtor Logo" /></Link>
+                            </li>
+                            <li className="main_title"><h2>Earthquake Zen Garden</h2></li>
+                            <li className="profile_title"><Link to={'/Sally_Profile_Page'}>Welcome {EarthquakeData.profile.firstName}</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+            </Router>
         );
     }
 }
