3efd2a796927c1e98248d09dbd0a58243d21f2f8
Reddy531 <chakradhar.lsg4@gmail.com>
2021-01-08 00:07:17 -0600

    updated folder structure

---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                Realtor Main Page
+            </div>
+        );
+    }
+}
+export default Main;
