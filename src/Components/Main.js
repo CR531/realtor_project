8e174a0ed4f7ed8f0f3bb3936415a746db20c1e2
Reddy531 <chakradhar.lsg4@gmail.com>
2021-01-08 01:27:52 -0600

    Added routing logic to child components

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Profile from "./Profile";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../CSS/project.css"
-class Main extends React.Component {
-    render() {
-        const { EarthquakeData } = this.props;
-        return (
-            <Router>
-                <nav className="navbar navbar-expand-lg navbar-light" style={{ "background": "lightgrey" }}>
-                    <div>
-                        <ul className="navbar-nav mr-auto">
-                            <li><Link to={'/'} className="nav-link">
-                                <img className="realtor_logo" src={EarthquakeData.site.logoImage} alt="Realtor Logo" /></Link>
-                            </li>
-                            <li className="main_title"><h2>Earthquake Zen Garden</h2></li>
-                            <li className="profile_title"><Link to={'/Sally_Profile_Page'}>Welcome {EarthquakeData.profile.firstName}</Link></li>
-                        </ul>
-                    </div>
-                </nav>
-            </Router>
-        );
-    }
+const Main = ({ EarthquakeData }) => {
+    return (
+        <Router>
+            <nav className="navbar navbar-expand-lg navbar-light" style={{ "background": "lightgrey" }}>
+                <ul className="navbar-nav mr-auto">
+                    <li><Link to={'/'} className="nav-link">
+                        <img className="realtor_logo" src={EarthquakeData.site.logoImage} alt="Realtor Logo" /></Link>
+                    </li>
+                    <li className="main_title"><h2>Earthquake Zen Garden</h2></li>
+                    <li className="profile_title"><Link to={'/Profile_Page'}><h6>Welcome {EarthquakeData.profile.firstName}</h6></Link></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route exact path="/" component={() => (<Redirect to='/Home_Page' />)} />
+                <Route path='/Home_Page' render={() => <Home featureCollectionData={EarthquakeData.data} />} />
+                <Route path='/Profile_Page' render={() => <Profile profileData={EarthquakeData.profile} />} />
+            </Switch>
+        </Router>
+    );
 }
 export default Main;
